37d53fd96d62cd9120eb6fa38806f9b46a7232fd
Rune Claeys <rune.claeys@live.be>
2021-11-28 19:40:11 +0100

    loading

---
+++ src/components/Home/GrandPrixHistory.jsx
@@ -0,0 +1,60 @@
+import { format, fromUnixTime } from 'date-fns';
+
+import { Col, ListGroup, Placeholder, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const GrandPrixHistory = ({ overview, loading }) => {
+   return (
+      <>
+         <h5 className="mb-0">History</h5>
+         {loading ? (
+            <ListGroup as="ol">
+               {[...Array(5)].map((_, i) => (
+                  <ListGroup.Item key={i} as="li" className="d-flex justify-content-between align-items-start">
+                     <Row className="w-100">
+                        <Placeholder className="d-flex align-items-center justify-content-center" xs={2} as={Col} animation="glow">
+                           <Placeholder className="rounded-1" xs={12} />
+                        </Placeholder>
+                        <Placeholder xs={9} as={Col} animation="glow">
+                           <Placeholder className="rounded-1" xs={8} />
+                           <Placeholder className="rounded-1" xs={10} />
+                        </Placeholder>
+                        <Placeholder as={Col} animation="glow" xs={1} className="d-flex align-items-center">
+                           <Placeholder className="rounded-1" xs={12} style={{ width: '20px' }} />
+                        </Placeholder>
+                     </Row>
+                  </ListGroup.Item>
+               ))}
+            </ListGroup>
+         ) : (
+            <ListGroup as="ol" reversed>
+               {overview.grand_prix
+                  .filter((gp) => fromUnixTime(gp.qualifying_start_timestamp) < new Date())
+                  .map((gp, i) => {
+                     return (
+                        <ListGroup.Item key={gp.id} as="li" className="d-flex justify-content-between align-items-start">
+                           <NavLink to={'/result/' + gp.id} className="w-100 text-decoration-none text-black">
+                              <Row className="w-100">
+                                 <Col className="d-flex align-items-center justify-content-center" xs={2}>
+                                    <strong>{i}</strong>
+                                 </Col>
+                                 <Col xs={9}>
+                                    <h6 className="p-0 mb-1">{gp.name}</h6>
+
+                                    <p className="m-0">{format(fromUnixTime(gp.qualifying_start_timestamp), 'dd/MM/yyyy HH:mm:ss')}</p>
+                                 </Col>
+                                 <Col xs={1} className="d-flex align-items-center">
+                                    <span>{gp?.userPrediction?.total_points || '-'}</span>
+                                 </Col>
+                              </Row>
+                           </NavLink>
+                        </ListGroup.Item>
+                     );
+                  })}
+            </ListGroup>
+         )}
+      </>
+   );
+};
+
+export default GrandPrixHistory;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useMemo } from 'react';
-import { Card, ListGroup, Stack, Row, Col, Spinner } from 'react-bootstrap';
-import { format, differenceInDays, fromUnixTime } from 'date-fns';
+import { Stack } from 'react-bootstrap';
+import { differenceInDays, fromUnixTime } from 'date-fns';
 
-import ShowcaseGP from '../components/ShowcaseGP';
+import ShowcaseGP from '../components/Home/ShowcaseGP';
 import { useGet } from '../core/hooks/useGet';
 import { API_RESULTS } from '../core/endpoints/endpoints';
+import SeasonTotal from '../components/Home/SeasonTotal';
+import GrandPrixHistory from '../components/Home/GrandPrixHistory';
 
 const Home = () => {
    const { data: overview, loading } = useGet(API_RESULTS, { initialValue: { grand_prix: [] } });
@@ -28,61 +30,11 @@ const Home = () => {
       return prevGP;
    }, [prevGP, nextGP]);
 
-   if (loading) return <div></div>;
-
    return (
       <Stack gap={3} className="mb-5">
-         {showcaseGP && <ShowcaseGP showcaseGP={showcaseGP} isUpcomming={showcaseGP?.id === nextGP?.id} loading={loading} />}
-
-         <h5 className="mb-0">overview results</h5>
-         <Card bg="secondary" text="white">
-            <Card.Body>
-               <Row>
-                  <Col className="text-center d-flex flex-column">
-                     <strong>Quali</strong>
-                     <span>{overview.qualifying_points}</span>
-                  </Col>
-                  <Col className="text-center d-flex flex-column">
-                     <strong>Race</strong>
-                     <span>{overview.race_points}</span>
-                  </Col>
-                  <Col className="text-center d-flex flex-column">
-                     <strong>Other</strong>
-                     <span>{overview.other_points}</span>
-                  </Col>
-                  <Col className="text-center d-flex flex-column">
-                     <strong>Total</strong>
-                     <span>{overview.total_points}</span>
-                  </Col>
-               </Row>
-            </Card.Body>
-         </Card>
-
-         <h5 className="mb-0">History</h5>
-         <ListGroup as="ol" reversed>
-            {overview.grand_prix
-               .filter((gp) => fromUnixTime(gp.qualifying_start_timestamp) < new Date())
-               .reverse()
-               .map((gp, i) => {
-                  return (
-                     <ListGroup.Item key={gp.id} as="li" className="d-flex justify-content-between align-items-start">
-                        <Row className="w-100">
-                           <Col className="d-flex align-items-center justify-content-center" xs={2}>
-                              <strong>{i}</strong>
-                           </Col>
-                           <Col xs={9}>
-                              <h6 className="p-0 mb-1">{gp.name}</h6>
-
-                              <p className="m-0">{format(fromUnixTime(gp.qualifying_start_timestamp), 'dd/MM/yyyy HH:mm:ss')}</p>
-                           </Col>
-                           <Col xs={1} className="d-flex align-items-center">
-                              <span>{gp?.userPrediction?.total_points || '-'}</span>
-                           </Col>
-                        </Row>
-                     </ListGroup.Item>
-                  );
-               })}
-         </ListGroup>
+         <ShowcaseGP showcaseGP={showcaseGP} isUpcomming={showcaseGP?.id === nextGP?.id} loading={loading} />
+         <SeasonTotal overview={overview} loading={loading} />
+         <GrandPrixHistory overview={overview} loading={loading} />
       </Stack>
    );
 };
