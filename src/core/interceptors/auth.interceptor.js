57ae6d975373a3808984f68e845faba51c00bd58
Rune Claeys <rune.claeys@live.be>
2021-12-05 15:04:13 +0100

    update axios

--- src/core/interceptors/auth.interceptor.js
+++ src/core/interceptors/auth.interceptor.js
@@ -3,14 +3,17 @@ import axios from 'axios';
 axios.interceptors.request.use(
    function (config) {
       const token = localStorage.getItem('bearerToken');
+
       if (!token) {
          return window.location.reload();
       }
 
+      config.withCredentials = false;
+
       config.headers = {
          ...config.headers,
          Authorization: `Bearer ${token}`,
-         // Accept: 'application/json',
+         Accept: 'application/json',
          'Content-Type': 'application/json',
       };
       return config;
