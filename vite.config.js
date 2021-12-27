85330ea3ac98f412d674471e03c6b0f3bec23da5
Rune Claeys <rune.claeys@live.be>
2021-12-27 22:28:50 +0100

    update images

--- vite.config.js
+++ vite.config.js
@@ -18,25 +18,25 @@ export default defineConfig({
             start_url: '/',
             display: 'standalone',
             background_color: '#e10600',
+            icons: [
+               {
+                  src: 'android-chrome-192x192.png',
+                  sizes: '192x192',
+                  type: 'image/png',
+               },
+               {
+                  src: '/android-chrome-512x512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+               },
+               {
+                  src: 'android-chrome-512x512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+                  purpose: 'any maskable',
+               },
+            ],
          },
-         icons: [
-            {
-               src: 'android-chrome-192x192.png',
-               sizes: '192x192',
-               type: 'image/png',
-            },
-            {
-               src: '/android-chrome-512x512.png',
-               sizes: '512x512',
-               type: 'image/png',
-            },
-            {
-               src: 'android-chrome-512x512.png',
-               sizes: '512x512',
-               type: 'image/png',
-               purpose: 'any maskable',
-            },
-         ],
       }),
    ],
    build: {
