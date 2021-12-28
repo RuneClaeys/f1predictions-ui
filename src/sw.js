65294bac7c6a630059d69ba0a0446d0725dca833
Rune Claeys <rune.claeys@live.be>
2021-12-28 10:23:55 +0100

    add cache control

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,23 @@
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 self.addEventListener('message', (event) => {
    if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+const apiURL = `^${import.meta.env.VITE_API_BASE_URL}/`;
+
+registerRoute(
+   new RegExp(apiURL),
+   new StaleWhileRevalidate({
+      plugins: [
+         new CacheableResponsePlugin({
+            statuses: [0, 200],
+         }),
+      ],
+   })
+);
