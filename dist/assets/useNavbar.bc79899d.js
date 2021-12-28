9bf7c2cd3eedbb741df66afc10531309e0fa58b8
Rune Claeys <rune.claeys@live.be>
2021-12-28 13:38:38 +0100

    add detail leaderboard

--- dist/assets/useNavbar.bc79899d.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./vendor.8eb74db1.js";import{a as t}from"./index.12a1afd9.js";function p(e){const{dispatch:r}=t();a.exports.useEffect(()=>(r({type:"SET_NAVBAR",payload:e}),()=>{r({type:"RESET_NAVBAR"})}),[e])}export{p as u};
