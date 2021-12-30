d9c71e62bacb722b5f179b4f9bff1dde6574f698
Rune Claeys <rune.claeys@live.be>
2021-12-30 12:28:09 +0100

    fallback lang

--- dist/assets/useNavbar.e89e55d3.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./vendor.761b0733.js";import{a}from"./index.274b0cca.js";function c(e){const{dispatch:r}=a();t.exports.useEffect(()=>(r({type:"SET_NAVBAR",payload:e}),()=>{r({type:"RESET_NAVBAR"})}),[e])}export{c as u};
